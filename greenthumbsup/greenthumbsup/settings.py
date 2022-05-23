93f44ec19e87f3c1c02a47836903045866e8372f
ruben <rubendasilva@me.com>
2022-05-23 14:43:30 -0700

    cross checked settings.py static url and stubbing imports for env usage

--- greenthumbsup/greenthumbsup/settings.py
+++ greenthumbsup/greenthumbsup/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os, environ
+environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
