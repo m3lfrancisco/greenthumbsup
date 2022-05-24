84890cf7760d8ea1dc720ee7699408a909649953
mfrancisco <francisco.melanie@gmail.com>
2022-05-24 15:45:34 -0700

    add FertilizingForm in forms.py, add_fertilizing to views.py, and urls.py

--- greenthumbsup/main_app/views.py
+++ greenthumbsup/main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .models import Plant, Fertilizer, Photo
-from .forms import WateringForm
+from .forms import WateringForm, FertilizingForm
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -99,19 +99,27 @@ def add_watering(request, plant_id):
         new_watering.save()
     return redirect('detail', plant_id=plant_id)
 
-# def add_photo(request, plant_id):
-#     photo_file = request.FILES.get('photo-file', None)
-#     if photo_file:
-#         s3 = boto3.client('s3')
-#         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-#         try:
-#             bucket = os.environ['S3_BUCKET']
-#             s3.upload_fileobj(photo_file, bucket, key)
-#             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-#             Photo.objects.create(url=url, plant_id=plant_id)
-#         except:
-#             print('An error occurred while uploading file to S3')
-#     return redirect('detail', plant_id=plant_id)
+def add_fertilizing(request, plant_id):
+    form = FertilizingForm(request.POST)
+    if form.is_valid():
+        new_fertilizing = form.save(commit=False)
+        new_fertilizing.plant_id = plant_id
+        new_fertilizing.save()
+    return redirect('detail', plant_id=plant_id)
+
+def add_photo(request, plant_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, plant_id=plant_id)
+        except:
+            print('An error occurred while uploading file to S3')
+    return redirect('detail', plant_id=plant_id)
 
 def assoc_fertilizer(request, plant_id, fertilizer_id):
     Plant.objects.get(id=plant_id).fertilizers.add(fertilizer_id)
