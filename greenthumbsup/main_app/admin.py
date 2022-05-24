11dc61ccf786577ccd8bde91b0674900c1fedbf7
Ruben <21150133+Roob19@users.noreply.github.com>
2022-05-24 12:59:18 -0700

    updating admin.py with registered models

--- greenthumbsup/main_app/admin.py
+++ greenthumbsup/main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Plant, Profile, Fertilizer, Photo, Watering
 
 # Register your models here.
+admin.site.register(Plant)
+admin.site.register(Profile)
+admin.site.register(Fertilizer)
+admin.site.register(Photo)
+admin.site.register(Watering)
