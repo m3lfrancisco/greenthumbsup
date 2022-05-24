74677f7858adeae888e0371c5b5d91923456f507
Ruben <21150133+Roob19@users.noreply.github.com>
2022-05-24 14:25:31 -0700

    making my_plants.html and edited signup view to render my_plants

--- greenthumbsup/main_app/views.py
+++ greenthumbsup/main_app/views.py
@@ -132,7 +132,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('my_plants')
         else:
             error_message = 'Invalid sign up, please try again'
     form = UserCreationForm()
