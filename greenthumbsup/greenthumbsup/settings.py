218845a38b5580fb407796b895b6360a41456922
mfrancisco <francisco.melanie@gmail.com>
2022-05-25 11:29:51 -0700

    add AWS and S3_BUCKET, install django-environ and boto3

--- greenthumbsup/greenthumbsup/settings.py
+++ greenthumbsup/greenthumbsup/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq8xb0g0epo1brqsd1+=2!h%#i+@fxn_ps#7$-^#xo&=sc)u1-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
