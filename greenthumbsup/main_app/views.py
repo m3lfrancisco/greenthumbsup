fe98d6dee788563ecba1eb21f8429ee9dd2a3215
mfrancisco <francisco.melanie@gmail.com>
2022-05-24 20:34:03 -0700

    add back FertilizerForm in forms.py, add_fertilizing in views.py and code html in my_plants.html

--- greenthumbsup/main_app/views.py
+++ greenthumbsup/main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .models import Plant, Fertilizer, Photo
-from .forms import WateringForm
+from .forms import WateringForm, FertilizingForm
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -56,7 +56,7 @@ def plants_detail(request, plant_id):
     return render(request, 'plants/detail.html', {
         'plant': plant, 
         'watering_form': WateringForm,
-        # 'fertilizing_form': FertilizingForm,
+        'fertilizing_form': FertilizingForm,
         'fertilizers': fertilizers_plant_doesnt_have
         })
 
@@ -98,6 +98,14 @@ def add_watering(request, plant_id):
         new_watering = form.save(commit=False)
         new_watering.plant_id = plant_id
         new_watering.save()
+    return redirect('detail', plant_id=plant_id)
+
+def add_fertilizing(request, plant_id):
+    form = FertilizingForm(request.POST)
+    if form.is_valid():
+        new_fertilizing = form.save(commit=False)
+        new_fertilizing.plant_id = plant_id
+        new_fertilizing.save()
     return redirect('detail', plant_id=plant_id)
 
 def add_photo(request, plant_id):
