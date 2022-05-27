abb056c1a4ecccdbe5cc89e9519ee958bd8b30f3
Ruben <21150133+Roob19@users.noreply.github.com>
2022-05-26 22:44:50 -0700

    quicker commit

--- pu.sh
+++ pu.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
+black=$'\e[0;30'
+red=$'\e[0;31'
+green$'\e[0;32'
+yellow=$'\e[0;33'
+blue=$'\e[0;34'
+Black=$'\e[1:30'
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Yellow=$'\e[1;33m'
+Blue=$'\e[1;34m'
 echo
 git add .
-echo "<><><><><><><><><><><><>"
+echo "$red <><><><><><><><><><><><>"
 echo
-echo "Enter commit message: "
+echo "$Red Enter commit message: "
 echo
-read cmtMsg
+read $red cmtMsg
 git commit -m "$cmtMsg"
 echo
-echo "<><><><><><><><><><><>"
+echo "$yellow <><><><><><><><><><><>"
 echo
 git branch
 echo
-echo “Type out the branch you wish to push to?”
+echo “$Yellow Type out the branch you wish to push to?”
 echo
-read pushBranch
+read $yellow pushBranch
 echo
 git push origin $pushBranch
-echo "<><><><><><><><><><><>"
+echo "$green <><><><><><><><><><><>"
 echo
-echo "Push complete :) "
+echo "$Green Push complete :) "
 echo
-echo "<><><><><><><><><><><>"
+echo "$blue <><><><><><><><><><><>"
 echo
-echo "Opening a browser tab for GH Pull Request..."
+echo "$Blue Opening a browser tab for GH Pull Request..."
 echo
 gh pr create -w
 echo
