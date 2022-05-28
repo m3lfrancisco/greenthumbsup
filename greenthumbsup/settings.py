c00d6d4288f5a8cbb381ad7308e5b497fa5a0d34
Ruben <21150133+Roob19@users.noreply.github.com>
2022-05-28 15:26:30 -0700

    DO NOT MERGE, delete any and all changes from this

--- greenthumbsup/settings.py
+++ greenthumbsup/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'greenthumbsup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'greenthumbsup',
-        'USER': 'myappuser',
-        'PASSWORD': 'mypass',
+        'NAME': 'd6m8krcajtvsps',
+        'USER': 'cdhakkjzinurlo',
+        'PASSWORD': 'bad0874ec726b02536b88e41d1b2323f90009a400da16743444e477bfc0bf4fb',
+        'HOST':'ec2-34-231-177-125.compute-1.amazonaws.com'
     }
 }
 
